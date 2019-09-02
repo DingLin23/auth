7dfb706df5d72f594b63774032e83dffeeb49546
Ding Lin <tim920323@gmail.com>
2019-09-02 02:12:30 -0400

    a...

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,81 @@
- import 'package:flutter/material.dart';
-//zz...
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as JSON;
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget{
-  State<StatefulWidget> createState(){
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
     return _MyAppState();
   }
 }
 
-class _MyAppState extends State<MyApp>{
-  bool isLoggedIn = true;
+class _MyAppState extends State<MyApp> {
+  bool _isLoggedIn = false;
   Map userProfile;
-  _loginWithFB(){
+  final facebookLogin = FacebookLogin();
+
+  _loginWithFB() async{
+
+    
+    final result = await facebookLogin.logInWithReadPermissions(['email']);
+
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final token = result.accessToken.token;
+        final graphResponse = await http.get('https://graph.facebook.com/v2.12/me?fields=name,picture,email&access_token=${token}');
+        final profile = JSON.jsonDecode(graphResponse.body);
+        print(profile);
+        setState(() {
+          userProfile = profile;
+          _isLoggedIn = true;
+        });
+        break;
+
+      case FacebookLoginStatus.cancelledByUser:
+        setState(() => _isLoggedIn = false );
+        break;
+      case FacebookLoginStatus.error:
+        setState(() => _isLoggedIn = false );
+        break;
+    }
 
   }
+
   _logout(){
-
+    facebookLogin.logOut();
+    setState(() {
+      _isLoggedIn = false;
+    });
   }
 
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-      body: Center(
-        child: isLoggedIn
-            ? Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-              Image.network("http://placehold.it/50x50",height: 50.0,width:50.0,),
-              Text(""),
-              OutlineButton(
-                child: Text("Logout"),
-                onPressed: (){},
-                )
-              ],
-            )
-          :OutlineButton(
-          child: Text("Login With Facebook"),
-          onPressed: (){},
+        body: Center(
+            child: _isLoggedIn
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.network(userProfile["picture"]["data"]["url"], height: 50.0, width: 50.0,),
+                      Text(userProfile["name"]),
+                      OutlineButton( child: Text("Logout"), onPressed: (){
+                        _logout();
+                      },)
+                    ],
+                  )
+                : Center(
+                    child: OutlineButton(
+                      child: Text("Login with Facebook"),
+                      onPressed: () {
+                        _loginWithFB();
+                      },
+                    ),
+                  )),
       ),
-      ),
-    ),
     );
   }
 }
